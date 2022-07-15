f4a99c30dfebbb7ad293153378583df73e0d5f4b
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-07-15 16:10:56 -0400

    Finished lesson 9-12

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+// let js ='amazing';
+// console.log(40+8+23-10);
+
+// let firstName = 'Tanzil'
+// console.log(firstName);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Tanzil');
+
+// javascriptIsFun = 'Yes!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null); //returns as object but really is undefined
