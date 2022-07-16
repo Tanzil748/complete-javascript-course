a7997f759f62fb7e8d97f949ccf6f6e0e2360c82
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-07-16 14:41:45 -0400

    Finish functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+ /*
 let hasDriverLicense = false;
 const passTest = true;
 
@@ -10,3 +10,24 @@ if(hasDriverLicense) console.log('I can drive');
 // const interface = 'Audio';
 // const private = 534;
 // const if = 1;
+*/
+
+function logger() {
+    console.log('My name is Tanzil');
+}
+
+//calling/running/invoking
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
