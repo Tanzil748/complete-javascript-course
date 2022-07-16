2eb99d10baac2ac8e3c5264cf35a5f0e2660f080
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-07-15 23:09:46 -0400

    Finish activating strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let hasDriverLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true;
+if(hasDriverLicense) console.log('I can drive');
+
+//not allowed to use certain keywords  in strict mode
+// const interface = 'Audio';
+// const private = 534;
+// const if = 1;
