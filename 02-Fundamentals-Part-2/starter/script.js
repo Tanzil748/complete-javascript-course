3bfc81ad6b39bfd15251274a0c53b318217e01bb
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-07-16 20:23:25 -0400

    Finish function calling other functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -47,7 +47,7 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1995);
 
 console.log(age1, age2);
-*/
+
 
 const calcAge2 = function (birthYear) {
     return 2037-birthYear;
@@ -66,3 +66,17 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 }
 
 console.log(yearsUntilRetirement(1991, 'Tanzil'));
+*/
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));
