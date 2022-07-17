dccfa3eb01a4f177383e23b3bdc7025021d7a422
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-07-16 20:16:39 -0400

    Finish arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -30,7 +30,6 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2,4);
 console.log(appleOrangeJuice);
-*/
 
 //function declaration
 function calcAge1(birthYear) {
@@ -48,3 +47,22 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1995);
 
 console.log(age1, age2);
+*/
+
+const calcAge2 = function (birthYear) {
+    return 2037-birthYear;
+}
+
+//arrow function
+const calcAge3 = birthYear => 2037-birthYear; //function name - parameter - expression
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Tanzil'));
