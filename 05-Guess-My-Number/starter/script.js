3ac85c81e34cd86185812b3ddc0c4b76d9314de1
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-07-19 19:14:56 -0400

    73. Handling Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number 🥳';
 // console.log(document.querySelector('.message').textContent);
@@ -9,3 +9,13 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Not a valid number 🚨';
+  }
+});
