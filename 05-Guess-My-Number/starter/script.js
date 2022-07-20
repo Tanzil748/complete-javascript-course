f2450bd1fae4a67ba02284467b9690d4572f0bb0
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-07-19 22:59:09 -0400

    Finish coding challenge 1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,9 +10,8 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -24,6 +23,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.score').textContent = score;
   } else if (guess === secretNumber && score > 0) {
     document.querySelector('.message').textContent = 'Correct number 👍';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber && score > 0) {
@@ -38,4 +38,15 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Game Over! 🥵';
     document.querySelector('body').style.backgroundColor = '#ff0000';
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20; //need this to reset score properly
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing ...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
