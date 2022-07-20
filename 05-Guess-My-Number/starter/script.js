0a2460a906583810e1e6b85c4b3dfe64ae7980fa
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-07-19 22:25:35 -0400

    74. Implementing Game Logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,12 +10,29 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  if (!guess) {
+  if (!guess && score > 0) {
     document.querySelector('.message').textContent = 'Not a valid number 🚨';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess === secretNumber && score > 0) {
+    document.querySelector('.message').textContent = 'Correct number 👍';
+  } else if (guess > secretNumber && score > 0) {
+    document.querySelector('.message').textContent = 'Guess is too high';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber && score > 0) {
+    document.querySelector('.message').textContent = 'Guess is too low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent = 'Game Over! 🥵';
   }
 });
