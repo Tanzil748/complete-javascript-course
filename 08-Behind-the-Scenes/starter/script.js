796ee48d3fabc1f070f523a9f6862cd1bae54dc4
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-08-07 15:19:32 -0400

    Scoping in practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+
+  function printAge() {
+    let output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 2001) {
+      const firstName = 'Steven';
+      const str = `Oh, and this is a milennial ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      output = 'New Output!';
+    }
+    //add(2, 3); //cannot run add in strict mode since out of scope
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Tanzil';
+
+calcAge(2000);
