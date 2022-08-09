62b2f1ddf489a39f6a2e42850c0e7066eefdfc35
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-08-09 18:19:48 -0400

    124.Coding Challenge 4

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -56,52 +56,101 @@ const restaurant = {
   },
 };
 ////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  // console.log(rows);
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first} ${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'🆗'.repeat(i + 1)}`);
+  }
+});
+
+////////////////////////////
 //Working with strings - part 3
 
 // Split and Join
-console.log('a+very+nice+string'.split('+'));
-console.log('Tanzil Hassan'.split(' '));
+// console.log('a+very+nice+string'.split('+'));
+// console.log('Tanzil Hassan'.split(' '));
 
-const [firstName, lastName] = 'Tanzil Hassan'.split(' ');
-console.log(firstName, lastName);
+// const [firstName, lastName] = 'Tanzil Hassan'.split(' ');
+// console.log(firstName, lastName);
 
-['Mr.', firstName, lastName.toUpperCase()].join(' ');
+// ['Mr.', firstName, lastName.toUpperCase()].join(' ');
 
-const capitalizeName = function (name) {
-  const names = name.split(' ');
-  const namesUpper = [];
+// const capitalizeName = function (name) {
+//   const names = name.split(' ');
+//   const namesUpper = [];
 
-  for (const n of names) {
-    namesUpper.push(n[0].toUpperCase() + n.slice(1));
-  }
-  console.log(namesUpper.join(' ')); //join makes it a string again
-};
+//   for (const n of names) {
+//     namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//   }
+//   console.log(namesUpper.join(' ')); //join makes it a string again
+// };
 
-capitalizeName('jessica ann smith davis');
-capitalizeName('tanzil hassan');
+// capitalizeName('jessica ann smith davis');
+// capitalizeName('tanzil hassan');
 
-//Padding
-const message = 'Go to gate 23!';
-console.log(message.padStart(25, '+').padEnd(35, '+'));
+// //Padding
+// const message = 'Go to gate 23!';
+// console.log(message.padStart(25, '+').padEnd(35, '+'));
 
-const maskCreditCard = function (number) {
-  const str = number + ''; //addition makes number string
-  const last = str.slice(-4);
-  return last.padStart(str.length, '*');
-};
+// const maskCreditCard = function (number) {
+//   const str = number + ''; //addition makes number string
+//   const last = str.slice(-4);
+//   return last.padStart(str.length, '*');
+// };
 
-console.log(maskCreditCard(389343493249));
-console.log(maskCreditCard('37846487'));
+// console.log(maskCreditCard(389343493249));
+// console.log(maskCreditCard('37846487'));
 
-//Repeat
-const message2 = 'Bad weather...All departures delayed... ';
-console.log(message2.repeat(5));
+// //Repeat
+// const message2 = 'Bad weather...All departures delayed... ';
+// console.log(message2.repeat(5));
 
-const planesInLine = function (n) {
-  console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
-};
+// const planesInLine = function (n) {
+//   console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+// };
 
-planesInLine(5);
+// planesInLine(5);
 
 /////////////////////////////
 //Working with strings - part 2
