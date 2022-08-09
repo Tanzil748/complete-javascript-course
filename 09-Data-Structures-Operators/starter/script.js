a0af1030729db9d116aae9bda3d4f30d5df4641a
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-08-09 13:27:23 -0400

    118. Maps - iterations

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -56,36 +56,74 @@ const restaurant = {
   },
 };
 
+////////////////////
+//Maps - iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C++'],
+  [2, 'Java'],
+  [3, 'Python'],
+  [4, 'Javascript'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try again!'],
+]);
+
+console.log(question);
+
+//convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+
+console.log(hoursMap);
+
+//Quiz App
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+//Convert map to array
+console.log([...question]);
+console.log(question.entries());
+console.log(question.keys());
+console.log(question.values());
+
 /////////////////////////////
-//Maps
-const rest = new Map();
-rest.set('name', 'Classico Subway');
-rest.set(1, 'NYC, NY');
-rest.set(2, 'Dallas, Texas');
+//Maps -fundamental
+// const rest = new Map();
+// rest.set('name', 'Classico Subway');
+// rest.set(1, 'NYC, NY');
+// rest.set(2, 'Dallas, Texas');
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open :)')
-  .set(false, 'We are closed :(');
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :)')
+//   .set(false, 'We are closed :(');
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
 
-const time = 21;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
-console.log(rest.has('categories'));
-rest.delete(2);
-// rest.clear();
-const arr = [1, 2];
-rest.set(arr, 'Test');
-rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest);
-console.log(rest.size);
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// // rest.clear();
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// console.log(rest.size);
 
-console.log(rest.get(arr));
+// console.log(rest.get(arr));
 
 //////////////////////////////////////
 // Sets: collection of unique values (can never have duplicates)
