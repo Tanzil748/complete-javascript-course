8b8ef7140413f6e31baaed9bbc015c6ce811c4c1
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-08-10 16:50:19 -0400

    129. How Passing Arg Works

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-//Default Parameter
+/*
+//128. Default Parameter
 const bookings = [];
 
 const createBooking = function (
@@ -20,3 +21,35 @@ const createBooking = function (
 createBooking('LH123');
 createBooking('LH123', 2, 20394);
 createBooking('LH123', undefined, 20394); //undefined lets the default value run if you want to skip a certain parameter
+*/
+
+//129. How Passing Arguments Work
+
+const flight = 'LH123';
+const tanzil = {
+  name: 'Tanzil Hassan',
+  passport: 123456789,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 123456789) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport number');
+  }
+};
+
+checkIn(flight, tanzil);
+
+console.log(flight);
+console.log(tanzil);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000);
+};
+
+newPassport(tanzil);
+checkIn(flight, tanzil);
