d5155ba9bae1b7e10d6d01b0a99f52dd929e8063
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-08-13 03:34:24 -0400

    150. the map method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -174,7 +174,7 @@ TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
 TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
-*/
+
 
 const dogsJulia = [3, 5, 2, 12, 7];
 const dogsKate = [4, 1, 15, 8, 3];
@@ -195,3 +195,31 @@ const checkDogs = function (dogsJulia, dogsKate) {
 checkDogs(dogsJulia, dogsKate);
 
 // console.log([...dogsJulia].slice(1).slice(0, 2));
+////////////////////////////////////
+*/
+// 150. map method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+
+const moveUSD = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
+
+console.log(movements);
+console.log(moveUSD);
+
+// const movementsDescriptions = movements.map((mov, i, arr) => {
+//   if (mov > 0) {
+//     return `Movement ${i + 1}: You deposited ${mov}`;
+//   } else {
+//     return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+//   }
+// });
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1} : You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDescriptions);
