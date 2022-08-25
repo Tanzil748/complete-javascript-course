f9be6523f09f2f516855f85bd5ec1b453e056acc
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-08-25 15:36:59 -0400

    Problem at [2:41] of lesson 188. s1coords not printing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -31,7 +31,7 @@ document.addEventListener('keydown', function (e) {
 });
 
 ////////////////////////////////////
-
+/*
 //Selecting Elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -118,3 +118,13 @@ logo.classList.contains('c');
 
 // DOnt use - it overrides
 logo.className = 'jonas';
+*/
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  // console.log(e.target.getBoundingClientRect());
+});
