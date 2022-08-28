fa878f87e6bf14aaadb0a5e87b5cb13759fad4dc
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-08-28 17:51:20 -0400

    223

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -457,27 +457,32 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    this._pin = pin;
+    // Protected property
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
   // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan Approved`);
     }
@@ -486,10 +491,11 @@ class Account {
 
 const acct1 = new Account('Tanzil', 'dollars', 1234);
 
-// acct1.movements.push(-140)
+// acct1._movements.push(-140)
 acct1.deposit(250);
 acct1.withdraw(-140);
 acct1.requestLoan(1000);
 acct1.approveLoan(1000);
+console.log(acct1.getMovements());
 console.log(acct1);
 console.log(acct1.pin);
